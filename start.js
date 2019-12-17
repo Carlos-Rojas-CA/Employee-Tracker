5e4561ea7f6892c046b4749eb15aae9888c94def
Carlos Rojas <c2rojas75@gmail.com>
2019-12-17 03:23:35 -0800

    First Commit: Working All employee option

---
+++ start.js
@@ -0,0 +1,232 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+const cTable = require('console.table');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "ZXCvbnASD!12345",
+    database: "employees_DB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    runSearch();
+});
+
+function runSearch() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View All Roles",
+                "View All Departments",
+                "Add Employee",
+                "Add Role",
+                "Add Department",
+                "Update Employee Roles"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View All Employees":
+                    console.log("here")
+                    employeePrint();
+                    break;
+
+                case "View All Roles":
+                    rolePrint();
+                    break;
+
+                case "View All Departments":
+                    departmentPrint();
+                    break;
+
+                case "Add Employee":
+                    addEmployee();
+                    break;
+
+                case "Add Role":
+                    addRole();
+                    break;
+
+                case "Add Department":
+                    addDepartment();
+                    break;
+                
+                case "Update Employee Roles":
+                    updateEmRole();
+                    break;
+    
+
+
+                case "Find artists with a top song and top album in the same year":
+                    songAndAlbumSearch();
+                    break;
+            }
+        });
+}
+
+function employeePrint(){
+    var query = 'SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, CONCAT( m.first_name, " ", m.last_name ) AS manager'
+    query += ' FROM employee e INNER JOIN role on e.role_id = role.id INNER JOIN department on role.department_id = department.id LEFT JOIN employee m ON e.manager_id = m.id;';
+    connection.query(query, function (err, res) {
+        console.table(res)
+        // console.log('hi')
+        // console.log("id\tfirst_name\tlast_name\ttitle\tdepartment\tsalary\tmanager")
+        // console.log("--\t----------\t---------\t-----\t----------\t------\t-------")
+        // res.forEach(el => {
+        //     console.log(el.id+ "\t" +el.first_name+ "\t\t" +el.last_name+ "\t" +el.role_id+ "\t" +el.manager_id);
+        // })
+
+        // for (var i = 0; i < res.length; i++) {
+        //     console.log(res[i].artist);
+        // }
+        // runSearch();
+    });
+}
+
+
+// function artistSearch() {
+//     inquirer
+//         .prompt({
+//             name: "artist",
+//             type: "input",
+//             message: "What artist would you like to search for?"
+//         })
+//         .then(function (answer) {
+//             var query = "SELECT position, song, year FROM top5000 WHERE ?";
+//             connection.query(query, { artist: answer.artist }, function (err, res) {
+//                 for (var i = 0; i < res.length; i++) {
+//                     console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+//                 }
+//                 runSearch();
+//             });
+//         });
+// }
+
+// function multiSearch() {
+//     var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
+//     connection.query(query, function (err, res) {
+//         for (var i = 0; i < res.length; i++) {
+//             console.log(res[i].artist);
+//         }
+//         runSearch();
+//     });
+// }
+
+// function rangeSearch() {
+//     inquirer
+//         .prompt([
+//             {
+//                 name: "start",
+//                 type: "input",
+//                 message: "Enter starting position: ",
+//                 validate: function (value) {
+//                     if (isNaN(value) === false) {
+//                         return true;
+//                     }
+//                     return false;
+//                 }
+//             },
+//             {
+//                 name: "end",
+//                 type: "input",
+//                 message: "Enter ending position: ",
+//                 validate: function (value) {
+//                     if (isNaN(value) === false) {
+//                         return true;
+//                     }
+//                     return false;
+//                 }
+//             }
+//         ])
+//         .then(function (answer) {
+//             var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
+//             connection.query(query, [answer.start, answer.end], function (err, res) {
+//                 for (var i = 0; i < res.length; i++) {
+//                     console.log(
+//                         "Position: " +
+//                         res[i].position +
+//                         " || Song: " +
+//                         res[i].song +
+//                         " || Artist: " +
+//                         res[i].artist +
+//                         " || Year: " +
+//                         res[i].year
+//                     );
+//                 }
+//                 runSearch();
+//             });
+//         });
+// }
+
+// function songSearch() {
+//     inquirer
+//         .prompt({
+//             name: "song",
+//             type: "input",
+//             message: "What song would you like to look for?"
+//         })
+//         .then(function (answer) {
+//             console.log(answer.song);
+//             connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function (err, res) {
+//                 console.log(
+//                     "Position: " +
+//                     res[0].position +
+//                     " || Song: " +
+//                     res[0].song +
+//                     " || Artist: " +
+//                     res[0].artist +
+//                     " || Year: " +
+//                     res[0].year
+//                 );
+//                 runSearch();
+//             });
+//         });
+// }
+
+// function songAndAlbumSearch() {
+//     inquirer
+//         .prompt({
+//             name: "artist",
+//             type: "input",
+//             message: "What artist would you like to search for?"
+//         })
+//         .then(function (answer) {
+//             var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
+//             query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
+//             query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
+
+//             connection.query(query, [answer.artist, answer.artist], function (err, res) {
+//                 console.log(res.length + " matches found!");
+//                 for (var i = 0; i < res.length; i++) {
+//                     console.log(
+//                         i + 1 + ".) " +
+//                         "Year: " +
+//                         res[i].year +
+//                         " Album Position: " +
+//                         res[i].position +
+//                         " || Artist: " +
+//                         res[i].artist +
+//                         " || Song: " +
+//                         res[i].song +
+//                         " || Album: " +
+//                         res[i].album
+//                     );
+//                 }
+
+//                 runSearch();
+//             });
+//         });
+// }
